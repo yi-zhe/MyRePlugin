3bc01baa4c1216c0f3a94bcbdbda818b2429da2a
l <l@com>
2023-04-07 14:54:53 +0800

    添加各种启动Activity的方式

--- Plugin/app/src/main/java/com/qihoo360/replugin/sample/demo1/MainActivity.java
+++ Plugin/app/src/main/java/com/qihoo360/replugin/sample/demo1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.qihoo360.replugin.sample.demo1;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.qihoo360.replugin.RePlugin;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +50,16 @@ public class MainActivity extends Activity {
             v.getContext().startActivity(intent);
         }));
 
+        mItems.add(new TestItem("Activity: SingleTop", v -> {
+            Intent intent = new Intent(v.getContext(), SingleTopActivity.class);
+            v.getContext().startActivity(intent);
+        }));
+
+        mItems.add(new TestItem("Activity: SingleInstance", v -> {
+            Intent intent = new Intent(v.getContext(), SingleInstanceActivity.class);
+            v.getContext().startActivity(intent);
+        }));
+
         mItems.add(new TestItem("Activity: Theme BlackNoTitleBar", v -> {
             Intent intent = new Intent(v.getContext(), ThemeBlackNoTitleBarActivity.class);
             v.getContext().startActivity(intent);
@@ -58,6 +72,37 @@ public class MainActivity extends Activity {
         mItems.add(new TestItem("Activity: Theme Dialog", v -> {
             Intent intent = new Intent(v.getContext(), ThemeDialogActivity.class);
             v.getContext().startActivity(intent);
+        }));
+
+        mItems.add(new TestItem("Activity: Open by Application", v -> {
+            Context app = v.getContext().getApplicationContext();
+            Intent intent = new Intent(app, ThemeDialogActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            app.startActivity(intent);
+        }));
+
+        mItems.add(new TestItem("Activity: By Intent Filter", v -> {
+            Intent intent = new Intent("android.intent.action.demo1");
+            intent.addCategory("category_demo");
+            v.getContext().startActivity(intent);
+        }));
+
+        mItems.add(new TestItem("Activity: By Action", v -> {
+            Intent intent = new Intent();
+            intent.setAction("com.qihoo360.replugin.sample.demo1.action.theme_fullscreen");
+            /*
+                若 Intent 中声明 category, manifest 中未声明，则无法找到 Activity;
+                若 manifest 中声明 category, Intent 中未声明，则可以找到 Activity;
+            */
+            intent.addCategory("com.qihoo360.repluginapp.replugin.demo1.CATEGORY1");
+            v.getContext().startActivity(intent);
+        }));
+
+        mItems.add(new TestItem("Activity: start Host activity(Main) NOT WORKING", v -> {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(RePlugin.getHostContext().getPackageName(), "com.qihoo360.replugin.sample.host.MainActivity"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            MainActivity.this.startActivity(intent);
         }));
     }
 
